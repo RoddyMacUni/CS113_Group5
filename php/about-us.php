3dc0fbf4f73e2a7a86c30d301f5118dcbc57e58f
kkb22133 <frangordon@hotmail.co.uk>
2023-06-20 10:36:51 +0100

    updated about us content

--- php/about-us.php
+++ php/about-us.php
@@ -11,25 +11,14 @@
 <body>
 <div class="aboutUsContainer">
     <img src="../images/logo.png" alt="" class="aboutUsImg">
-    <p class="aboutUsText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus vehicula sodales. 
-        Sed quis rutrum risus, at egestas orci. Mauris congue est metus, in vestibulum sapien iaculis a. 
-        Donec bibendum mi et urna porttitor, vitae tempor libero condimentum. Fusce urna arcu, molestie in lorem iaculis, consectetur tincidunt dui. 
-        Nam consectetur lacus non ex gravida efficitur. Donec vehicula ullamcorper laoreet. Cras elit ipsum, accumsan vitae velit eget, varius accumsan nisl.
-        Sed quis rutrum risus, at egestas orci. Mauris congue est metus, in vestibulum sapien iaculis a. 
-        Donec bibendum mi et urna porttitor, vitae tempor libero condimentum. Fusce urna arcu, molestie in lorem iaculis, consectetur tincidunt dui. 
-        Nam consectetur lacus non ex gravida efficitur. Donec vehicula ullamcorper laoreet. Cras elit ipsum, accumsan vitae velit eget, varius accumsan nisl.
-        Sed quis rutrum risus, at egestas orci. Mauris congue est metus, in vestibulum sapien iaculis a. 
-        Donec bibendum mi et urna porttitor, vitae tempor libero condimentum. Fusce urna arcu, molestie in lorem iaculis, consectetur tincidunt dui. 
-        Nam consectetur lacus non ex gravida efficitur. Donec vehicula ullamcorper laoreet. Cras elit ipsum, accumsan vitae velit eget, varius accumsan nisl.
-        Sed quis rutrum risus, at egestas orci. Mauris congue est metus, in vestibulum sapien iaculis a. 
-        Donec bibendum mi et urna porttitor, vitae tempor libero condimentum. Fusce urna arcu, molestie in lorem iaculis, consectetur tincidunt dui. 
-        Nam consectetur lacus non ex gravida efficitur. Donec vehicula ullamcorper laoreet. Cras elit ipsum, accumsan vitae velit eget, varius accumsan nisl.
-        Sed quis rutrum risus, at egestas orci. Mauris congue est metus, in vestibulum sapien iaculis a. 
-        Donec bibendum mi et urna porttitor, vitae tempor libero condimentum. Fusce urna arcu, molestie in lorem iaculis, consectetur tincidunt dui. 
-        Nam consectetur lacus non ex gravida efficitur. Donec vehicula ullamcorper laoreet. Cras elit ipsum, accumsan vitae velit eget, varius accumsan nisl.</p>
+    <p class="aboutUsText">Welcome to our website, a place where kids discover the exciting world of coding and unleash their creativity!
+        We believe that coding is not only a valuable skill but also a fantastic outlet for self-expression and problem-solving. Our goal is to provide a safe and engaging platform where children can learn the foundations of coding while having a ton of fun along the way.
+        In today's fast-paced, tech-driven world, coding skills have become increasingly important. Our carefully designed curriculum introduces kids to coding concepts through interactive lessons, hands-on projects, and playful activities. By developing their logical thinking, critical reasoning, and computational skills, we empower young minds to thrive in the digital age.
+        What sets us apart is our commitment to creating a positive and nurturing environment. We take online safety seriously, ensuring that all content is age-appropriate and reviewed for suitability. Our community is moderated, fostering a friendly space where kids can connect, collaborate, and celebrate their achievements.
+        Our team consists of passionate educators and experienced coders who know how to make learning a blast. We believe that education should be exciting and enjoyable. That's why we infuse our lessons with creativity, colorful visuals, and interactive challenges. We want kids to discover the joy of coding while building their confidence and unleashing their imagination.
+        Whether your child dreams of creating websites, designing games, or simply exploring the fascinating world of coding, our website is here to support their journey. We offer a comprehensive learning experience that encourages curiosity, fosters creativity, and prepares them for a future filled with endless possibilities.
+        Join us on this amazing coding adventure! Sign up today and let the coding fun begin!</p>
 </div>
-
-
 <div class="pageDivider">Need more info?</div>
 
 
