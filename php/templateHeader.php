cc61f16bbe9624aa0cedeef0fe8ddf0ee7963d14
blairwilson-student <blair.wilson.2022@uni.strath.ac.uk>
2023-06-20 08:24:06 +0100

    Add Learn page to navbar

--- php/templateHeader.php
+++ php/templateHeader.php
@@ -8,6 +8,7 @@
                     <ul class="nav-list" >
                         <li><a href="index.php">Home</a></li>
                         <li><a href="about-us.php">About</a></li>
+                        <li><a href="learn.php">Learn</a></li>
                         <li><a href="MoreButtons.php">More Buttons</a></li>
                         <li><a href="login.php">Log in</a></li>
                     </ul>
