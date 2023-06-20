ee822e805de3c15be901255a0d83373448d3e098
Roddy MacRae <roderick.macrae.2022@uni.strath.ac.uk>
2023-06-20 08:59:52 +0100

    Added more sections to homepage

--- php/index.php
+++ php/index.php
@@ -7,39 +7,51 @@
     <link rel="stylesheet" href="../css/stylesheet.css">
     <?php include 'templateHeader.php'; ?>
 </head>
-<br>
 <body>
-<div class="image-container">
-    <div class="image">
-        <img class="image" src="../images/placeholder.jpg">
-        <br>
-        <p>Image</p>
+    <br>
+
+    <div class="image-container">
+        <div class="image">
+            <img class="image" src="../images/placeholder.jpg">
+            <br>
+            <p>Image</p>
+        </div>
+        <div class="image">
+            <img class="image" src="../images/placeholder.jpg">
+            <br>
+            <p>Image</p>
+        </div>
+        <div class="image">
+            <img class="image" src="../images/placeholder.jpg">
+            <br>
+            <p>Image</p>
+        </div>
     </div>
-    <div class="image">
-        <img class="image" src="../images/placeholder.jpg">
-        <br>
-        <p>Image</p>
+
+
+    <div class="banner">
+        <img class = "banner" src="../images/placeholder.jpg">
     </div>
-    <div class="image">
-        <img class="image" src="../images/placeholder.jpg">
-        <br>
-        <p>Image</p>
+
+    <div class="video-container">
+        <video src="test.mp4" controls>
+        </video>
     </div>
-</div>
 
+    <div name="homepage-login-section">
+        Already a member?
+        <button type="button"><a href="login.php">Log in</a> </button>
+    </div>
 
-<div class="banner">
-    <img class = "banner" src="../images/placeholder.jpg">
-</div>
+    <div class="video-container">
+        <img src="../images/placeholder.jpg">
+    </div>
 
-<div class="video-container">
-    <video src="test.mp4" controls>
-    </video>
-</div>
+    <div name="homepage-FAQ-section">
+        Got a question?
+        <button type="button"><a href="faq.php">FAQ</a> </button>
+    </div>
 
-<div class="video-container">
-    <img src="../images/placeholder.jpg">
-</div>
 
     <?php include 'templateFooter.php'; ?>
 </body>
