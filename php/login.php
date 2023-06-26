ccd76d44dc76c4a3d87ddc77b0dfcb7585ac23af
Roddy MacRae <roderick.macrae.2022@uni.strath.ac.uk>
2023-06-26 08:18:08 +0100

    Added login functionality with no db access

--- php/login.php
+++ php/login.php
@@ -9,12 +9,61 @@
 </head>
 <body>
 
-<form action="handleLogin.php" method="post" class="loginForm">
+<form method="post" class="loginForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
     <input type="text" name="username" placeholder="Username" class="loginItem" required> <br>
-    <input type="password" name="password" placeholder="Password" class="loginItem" required> <br>
+
+    <?php
+    if($_SERVER["REQUEST_METHOD"] == "POST" && count($_POST) > 0) {
+
+        if(!validateUsernamePassword()) {
+            echo <<<END
+                <style>
+                input[name=password] {
+                    border: red solid 0.2rem;
+                    color: red;
+                }
+                
+                p[name=password-incorrect] {
+                    padding-top: 0;
+                    margin-top: 0;
+                    color: red;
+                }
+                </style>
+                
+                <input type="password" name="password" placeholder="Password incorrect" class="loginItem" required>
+                <p name="password-incorrect"> Password incorrect, please try again</p>
+                END;
+        }
+        else {
+            header("Location: parent-zone.php");
+            exit();
+        }
+    }
+    else {
+        echo '<input type="password" name="password" placeholder="Password" class="loginItem" required> <br>';
+    }
+    ?>
+
     <input type="submit" value="Log in" class="loginButton">
 </form>
 
 <?php include 'templateFooter.php'; ?>
 </body>
 </html>
+
+<?php
+
+function validateUsernamePassword(): bool
+{
+
+    $sanitizedUsername = strip_tags($_POST["username"]);
+    $sanitizedPassword = strip_tags($_POST["password"]);
+
+    return ($sanitizedPassword === getUserDetails($sanitizedUsername));
+}
+
+//TODO Fetch username and password from db for milestone 3
+function getUserDetails($username): string
+{
+    return "password";
+}
